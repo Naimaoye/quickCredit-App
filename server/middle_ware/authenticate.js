c5c33b090010b3fbbab84852c47187c78623e914
Naimaoye <naimatoyewale@gmail.com>
2019-05-16 14:44:35 +0100

    (chore):add middleware files,model files and routes

--- server/middle_ware/authenticate.js
+++ server/middle_ware/authenticate.js
@@ -6,34 +6,34 @@ import jwt from 'jsonwebtoken';
 dotenv.config();
 
 class Authenticator {
-   
-  static generateToken(payload) {
-    return jwt.sign(
-             { payload }, 
-             process.env.JWT_SECRET_KEY, 
+    static generateToken(payload) {
+        return jwt.sign(
+            { payload },
+            process.env.JWT_SECRET_KEY,
             { expiresIn: '12h' }
-      );
-  }
+        );
+    }
 
-  
-  static verifyToken(token) {
-    return jwt.verify(
-             token, 
-             process.env.JWT_SECRET_KEY
-      );
-  }
 
-  static hashPassword(password) {
-    return bcrypt.hashSync(
-           password, 
-           bcrypt.genSaltSync(10));
-  }
- 
-  static comparePassword(hashPassword, password) {
-    return bcrypt.compareSync(
-              password, 
-              hashPassword
-      );
-  }
+    static verifyToken(token) {
+        return jwt.verify(
+            token,
+            process.env.JWT_SECRET_KEY
+        );
+    }
+
+    static hashPassword(password) {
+        return bcrypt.hashSync(
+            password,
+            bcrypt.genSaltSync(10)
+        );
+    }
+
+    static comparePassword(hashPassword, password) {
+        return bcrypt.compareSync(
+            password,
+            hashPassword
+        );
+    }
 }
 export default Authenticator;
