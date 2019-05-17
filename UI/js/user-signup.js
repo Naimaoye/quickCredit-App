58d53b8adf70f21fdfbcf34f3cde6ac3f345d950
Naimaoye <naimatoyewale@gmail.com>
2019-05-17 12:02:34 +0100

    add validation

--- UI/js/user-signup.js
+++ UI/js/user-signup.js
@@ -1,16 +1,14 @@
-var ck_name = /^[A-Za-z0-9 ]{3,20}$/;
-var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]
+let ck_name = /^[A-Za-z0-9 ]{3,20}$/;
+let ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]
 {0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i 
-var ck_username = /^[A-Za-z0-9_]{1,20}$/;
-var ck_password =  /^[A-Za-z0-9!@#$%^&*()_]{6,20}$/;
+let ck_username = /^[A-Za-z0-9_]{1,20}$/;
+let ck_password =  /^[A-Za-z0-9!@#$%^&*()_]{6,20}$/;
 
 function validate(form){
-var name = form.name.value;
-var email = form.email.value;
-var username = form.username.value;
-var password = form.password.value;
-var gender = form.gender.value;
-var errors = [];
+let name = document.getElementById('').value;
+let email = document.getElementById('').value;
+let password = document.getElementById('').value;
+let errors = [];
  
  if (!ck_name.test(name)) {
   errors[errors.length] = "You valid Name .";
@@ -18,15 +16,10 @@ var errors = [];
  if (!ck_email.test(email)) {
   errors[errors.length] = "You must enter a valid email address.";
  }
- if (!ck_username.test(username)) {
-  errors[errors.length] = "You valid UserName no special char .";
- }
  if (!ck_password.test(password)) {
   errors[errors.length] = "You must enter a valid Password ";
  }
- if (gender==0) {
-  errors[errors.length] = "Select Gender";
- }
+ 
  if (errors.length > 0) {
 
   reportErrors(errors);
